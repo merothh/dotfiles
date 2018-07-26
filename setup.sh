e85725b1bac3e165a44984a6cb0da3b48bc0c8a7
merothh <merothh@gmail.com>
2018-07-26 20:47:18 +0530

    urxvt: fonts: use Inconsolata for Powerline
      * patched fonts pulled from https://github.com/powerline/fonts/blob/master/Inconsolata/Inconsolata%20for%20Powerline.otf
      * plays nice with oh-my-zsh and urxvt (to some degree atlest)

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ case $release in
 		sudo pacman -Sy i3-wm i3blocks i3lock dmenu scrot alsa-utils sysstat acpi feh playerctl xorg-xbacklight rxvt-unicode urxvt-perls adapta-gtk-theme papirus-icon-theme lxappearance
 
 		#cleanup leftovers
-		rm -rf ~/.config/i3 ~/.i3 ~/.fonts/Font-Awesome
+		rm -rf ~/.config/i3 ~/.i3 ~/.fonts/{Font-Awesome,Inconsolata-for-Powerline}
 
 		#make sure directories are present
 		mkdir -p ~/{.fonts,.config}
@@ -21,6 +21,7 @@ case $release in
 		#symlink all the goodies
 		ln -s ~/dotfiles/.config/i3/ ~/.config/i3 
 		ln -s ~/dotfiles/.fonts/Font-Awesome/ ~/.fonts/Font-Awesome
+		ln -s ~/dotfiles/.fonts/Inconsolata-for-Powerline ~/.fonts/Inconsolata-for-Powerline
 		ln -s ~/dotfiles/.Xresources ~/.Xresources
 
 		echo -e "\n$($cyan)All done. Make sure to set themes and fonts using $($yellow)lxappearance $($cyan)after logging into i3wm"
