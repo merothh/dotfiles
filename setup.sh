11b24968b2b07074d86bbef68e3373c38602094b
merothh <merothh@gmail.com>
2018-07-26 16:19:36 +0530

    dotfiles: setup.sh: add a ending note / color the output

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+cyan='tput setaf 6'
+yellow='tput setaf 3'
+
 release=$(head /etc/os-release -n 1 | cut -d '"' -f 2)
 
 case $release in 
 	"Arch Linux"|"Antergos Linux"|"Manjaro Linux")
 
-		echo "Installing required packages"
+		echo -e "\n$($cyan)Installing required packages"
 		sudo pacman -Sy i3-wm i3blocks i3lock dmenu scrot alsa-utils sysstat acpi feh playerctl xorg-xbacklight rxvt-unicode urxvt-perls adapta-gtk-theme papirus-icon-theme lxappearance
 
 		#cleanup leftovers
@@ -20,9 +23,10 @@ case $release in
 		ln -s ~/dotfiles/.fonts/Font-Awesome/ ~/.fonts/Font-Awesome
 		ln -s ~/dotfiles/.Xresources ~/.Xresources
 
+		echo -e "\n$($cyan)All done. Make sure to set themes and fonts using $($yellow)lxappearance $($cyan)after logging into i3wm"
 	;;
 	*)
-		echo "woops. you're not running an Arch based distro"
+		echo -e "\n$($cyan)woops. you're not running an Arch based distro"
 	;;
 
 esac
