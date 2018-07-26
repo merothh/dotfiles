8cef8d5efef0746b50216ef05abe0a6600b50168
merothh <merothh@gmail.com>
2018-07-26 15:23:59 +0530

    dotfiles: setup.sh: cleanup

--- setup.sh
+++ setup.sh
@@ -8,15 +8,20 @@ case $release in
 		echo "Installing required packages"
 		sudo pacman -Sy i3-wm i3blocks i3lock dmenu scrot alsa-utils sysstat acpi feh playerctl xorg-xbacklight rxvt-unicode urxvt-perls adapta-gtk-theme papirus-icon-theme lxappearance
 
+		#cleanup leftovers
 		rm -rf ~/.config/i3 ~/.i3 ~/.fonts/Font-Awesome
 
+		#make sure directories are present
+		mkdir -p ~/{.fonts,.config}
+
+		#symlink all the goodies
 		ln -s ~/dotfiles/.config/i3/ ~/.config/i3 
-		mkdir -p ~/.fonts && ln -s ~/dotfiles/.fonts/Font-Awesome/ ~/.fonts/Font-Awesome
+		ln -s ~/dotfiles/.fonts/Font-Awesome/ ~/.fonts/Font-Awesome
 		ln -s ~/dotfiles/.Xresources ~/.Xresources
 
 	;;
 	*)
-		echo "meh. you're not running an Arch based distro"
+		echo "woops. you're not running an Arch based distro"
 	;;
 
 esac
