4447c4fac41e034645c647455bc8597677b17097
merothh <merothh@gmail.com>
2018-08-17 13:15:44 +0530

    dotfiles: setup.sh: conditionally install zsh + oh-my-zsh-git

--- setup.sh
+++ setup.sh
@@ -6,14 +6,23 @@ reset='tput sgr0'
 
 release=$(head /etc/os-release -n 1 | cut -d '"' -f 2)
 
+echo -e "\n$($cyan)Switch to $($yellow)zsh $($cyan)+ $($yellow)oh-my-zsh-git $($cyan)? [y/n]$($reset)?"
+read zsh
+
+if [ "$zsh" = "y" ]; then
+	packages+="zsh git base-devel"
+fi
+
 case $release in 
 	"Arch Linux"|"Antergos Linux"|"Manjaro Linux")
 
+		packages+=" i3-wm i3blocks i3lock dmenu scrot alsa-utils sysstat acpi feh playerctl xorg-xbacklight rxvt-unicode urxvt-perls adapta-gtk-theme papirus-icon-theme lxappearance"
+
 		echo -e "\n$($cyan)Installing required packages$($reset)\n"
-		sudo pacman -Sy i3-wm i3blocks i3lock dmenu scrot alsa-utils sysstat acpi feh playerctl xorg-xbacklight rxvt-unicode urxvt-perls adapta-gtk-theme papirus-icon-theme lxappearance
+		sudo pacman -S $packages
 
 		#cleanup leftovers
-		rm -rf ~/.config/i3 ~/.i3 ~/.fonts/{Font-Awesome,Inconsolata-for-Powerline}
+		rm -rf ~/.config/i3 ~/{.i3,.zshrc} ~/.fonts/{Font-Awesome,Inconsolata-for-Powerline}
 
 		#make sure directories are present
 		mkdir -p ~/{.fonts,.config}
@@ -25,7 +34,25 @@ case $release in
 		ln -s ~/dotfiles/.fonts/Inconsolata-for-Powerline ~/.fonts/Inconsolata-for-Powerline
 		ln -s ~/dotfiles/.Xresources ~/.Xresources
 
-		echo -e "\n$($cyan)All done. Make sure to set themes and fonts using $($yellow)lxappearance $($cyan)after logging into i3wm\n"
+		#setup zsh & oh-my-zsh
+		if [ "$zsh" = "y" ]; then
+			echo -e "\n$($cyan)Changing default shell to $($yellow)zsh$($reset)\n"
+			chsh -s $(which zsh)
+
+			echo -e "\n$($cyan)Cloning & Building $($yellow)oh-my-zsh-git$($reset)\n"
+			git clone https://aur.archlinux.org/oh-my-zsh-git .build
+			cd .build && makepkg -si
+			cd ../
+			rm -rf .build
+
+			echo -e "\n$($cyan)Backing up any previous $($yellow).zshrc $($cyan)& symlinking new one$($reset)\n"
+			if [ -f ~/.zshrc ]; then
+				mv ~/.zshrc ~/.zshrc.old
+			fi
+			ln -s ~/dotfiles/.zshrc ~/.zshrc
+		fi
+
+		echo -e "\n$($cyan)All done. Make sure to \n	1.Set themes and fonts using $($yellow)lxappearance $($cyan)after logging into i3wm\n	2. Maybe move useful code from $($yellow).zshrc.old $($cyan)or $($yellow).bashrc"
 	;;
 	*)
 		echo -e "\n$($cyan)woops. you're not running an Arch based distro\n"
