97b74d748f18d3e3bfa291eb4fabd9bb84262e24
merothh <merothh@gmail.com>
2018-08-17 13:41:19 +0530

    dotfiles: setup.sh: a bit of output formatting

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ reset='tput sgr0'
 
 release=$(head /etc/os-release -n 1 | cut -d '"' -f 2)
 
-echo -e "\n$($cyan)Switch to $($yellow)zsh $($cyan)+ $($yellow)oh-my-zsh-git $($cyan)? [y/n]$($reset)?"
+echo -e "\n$($cyan)// Switch to $($yellow)zsh $($cyan)+ $($yellow)oh-my-zsh-git $($cyan)? [y/n]$($reset)?"
 read zsh
 
 if [ "$zsh" = "y" ]; then
@@ -18,7 +18,7 @@ case $release in
 
 		packages+=" i3-wm i3blocks i3lock dmenu scrot alsa-utils sysstat acpi feh playerctl xorg-xbacklight rxvt-unicode urxvt-perls adapta-gtk-theme papirus-icon-theme lxappearance"
 
-		echo -e "\n$($cyan)Installing required packages$($reset)\n"
+		echo -e "\n$($cyan)// Installing required packages$($reset)\n"
 		sudo pacman -S $packages
 
 		#cleanup leftovers
@@ -33,7 +33,7 @@ case $release in
 		ln -s ~/dotfiles/.fonts/Font-Awesome/ ~/.fonts/Font-Awesome
 		ln -s ~/dotfiles/.fonts/Inconsolata-for-Powerline ~/.fonts/Inconsolata-for-Powerline
 
-		echo -e "\n$($cyan)Backing up any previous $($yellow).Xresources $($cyan)& symlinking new one$($reset)\n"
+		echo -e "\n$($cyan)// Backing up any previous $($yellow).Xresources $($cyan)& symlinking new one$($reset)\n"
 		if [ -f ~/.Xresources ]; then
 			mv ~/.Xresources ~/.Xresources.old
 		fi
@@ -41,26 +41,26 @@ case $release in
 
 		#setup zsh & oh-my-zsh
 		if [ "$zsh" = "y" ]; then
-			echo -e "\n$($cyan)Changing default shell to $($yellow)zsh$($reset)\n"
+			echo -e "\n$($cyan)// Changing default shell to $($yellow)zsh$($reset)\n"
 			chsh -s $(which zsh)
 
-			echo -e "\n$($cyan)Cloning & Building $($yellow)oh-my-zsh-git$($reset)\n"
+			echo -e "\n$($cyan)// Cloning & Building $($yellow)oh-my-zsh-git$($reset)\n"
 			git clone https://aur.archlinux.org/oh-my-zsh-git .build
 			cd .build && makepkg -si
 			cd ../
 			rm -rf .build
 
-			echo -e "\n$($cyan)Backing up any previous $($yellow).zshrc $($cyan)& symlinking new one$($reset)\n"
+			echo -e "\n$($cyan)// Backing up any previous $($yellow).zshrc $($cyan)& symlinking new one$($reset)\n"
 			if [ -f ~/.zshrc ]; then
 				mv ~/.zshrc ~/.zshrc.old
 			fi
 			ln -s ~/dotfiles/.zshrc ~/.zshrc
 		fi
 
-		echo -e "\n$($cyan)All done. Make sure to \n	1.Set themes and fonts using $($yellow)lxappearance $($cyan)after logging into i3wm\n	2. Maybe move useful code from $($yellow).zshrc.old $($cyan)or $($yellow).bashrc\n	$($cyan)3.Keep your old $($yellow).Xresources.old $($cyan)tucked off somewhere"
+		echo -e "\n$($cyan)// All done. Make sure to \n	1.Set themes and fonts using $($yellow)lxappearance $($cyan)after logging into i3wm\n	2.Maybe move useful code from $($yellow).zshrc.old $($cyan)or $($yellow).bashrc\n	$($cyan)3.Keep your old $($yellow).bashrc .zshrc.old .Xresources.old $($cyan)tucked off somewhere"
 	;;
 	*)
-		echo -e "\n$($cyan)woops. you're not running an Arch based distro\n"
+		echo -e "\n$($cyan)// woops. you're not running an Arch based distro\n"
 	;;
 
 esac
