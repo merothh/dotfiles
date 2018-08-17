79cd44e362a2535eca5b5a30952a5299ccd56207
merothh <merothh@gmail.com>
2018-08-18 00:41:48 +0530

    dotfiles: setup.sh: refactor/support ubuntu based distros too

--- setup.sh
+++ setup.sh
@@ -9,10 +9,6 @@ release=$(head /etc/os-release -n 1 | cut -d '"' -f 2)
 echo -e "\n$($cyan)// Switch to $($yellow)zsh $($cyan)+ $($yellow)oh-my-zsh-git $($cyan)? [y/n]$($reset)?"
 read zsh
 
-if [ "$zsh" = "y" ]; then
-	packages+="zsh git base-devel"
-fi
-
 case $release in 
 	"Arch Linux"|"Antergos Linux"|"Manjaro Linux")
 
@@ -21,28 +17,11 @@ case $release in
 		echo -e "\n$($cyan)// Installing required packages$($reset)\n"
 		sudo pacman -S $packages
 
-		#cleanup leftovers
-		rm -rf ~/.config/i3 ~/{.i3,.zshrc} ~/.fonts/{Font-Awesome,Inconsolata-for-Powerline}
-
-		#make sure directories are present
-		mkdir -p ~/{.fonts,.config}
-		mkdir -p ~/Pictures/Screenshots
-
-		#symlink all the goodies
-		ln -s ~/dotfiles/.config/i3/ ~/.config/i3 
-		ln -s ~/dotfiles/.fonts/Font-Awesome/ ~/.fonts/Font-Awesome
-		ln -s ~/dotfiles/.fonts/Inconsolata-for-Powerline ~/.fonts/Inconsolata-for-Powerline
-
-		echo -e "\n$($cyan)// Backing up any previous $($yellow).Xresources $($cyan)& symlinking new one$($reset)\n"
-		if [ -f ~/.Xresources ]; then
-			mv ~/.Xresources ~/.Xresources.old
-		fi
-		ln -s ~/dotfiles/.Xresources ~/.Xresources
-
-		#setup zsh & oh-my-zsh
+		#build oh-my-zsh-git
 		if [ "$zsh" = "y" ]; then
-			echo -e "\n$($cyan)// Changing default shell to $($yellow)zsh$($reset)\n"
-			chsh -s $(which zsh)
+
+			echo -e "\n$($cyan)Installing dependencies for building $(yellow) oh-my-zsh-git$($reset)\n"
+			sudo pacman -S zsh git base-devel
 
 			echo -e "\n$($cyan)// Cloning & Building $($yellow)oh-my-zsh-git$($reset)\n"
 			git clone https://aur.archlinux.org/oh-my-zsh-git .build
@@ -50,17 +29,62 @@ case $release in
 			cd ../
 			rm -rf .build
 
-			echo -e "\n$($cyan)// Backing up any previous $($yellow).zshrc $($cyan)& symlinking new one$($reset)\n"
-			if [ -f ~/.zshrc ]; then
-				mv ~/.zshrc ~/.zshrc.old
-			fi
-			ln -s ~/dotfiles/.zshrc ~/.zshrc
 		fi
+	;;
+	"Ubuntu")
+		echo -e "\n$($cyan)// Adding PPA for $($yellow) adapta-gtk-theme $($cyan) && $($yellow) papirus-icon-theme$($reset)\n"
+		sudo add-apt-repository ppa:papirus/papirus
+		sudo add-apt-repository ppa:tista/adapta
 
-		echo -e "\n$($cyan)// All done. Make sure to \n	1.Set themes and fonts using $($yellow)lxappearance $($cyan)after logging into i3wm\n	2.Maybe move useful code from $($yellow).zshrc.old $($cyan)or $($yellow).bashrc\n	$($cyan)3.Keep your old $($yellow).bashrc .zshrc.old .Xresources.old $($cyan)tucked off somewhere"
+		packages+=" i3-wm i3blocks i3lock suckless-tools scrot alsa-utils sysstat acpi feh xbacklight rxvt-unicode adapta-gtk-theme papirus-icon-theme lxappearance"
+
+		echo -e "\n$($cyan)// Installing required packages$($reset)\n"
+		sudo apt install $packages
+
+		#clone/install oh-my-zsh
+		if [ "$zsh" = "y" ]; then
+
+			echo -e "\n$($cyan)// Installing $($yellow)oh-my-zsh$($reset)\n"
+			sudo apt install git zsh
+			sudo rm -rf ~/.oh-my-zsh /usr/share/oh-my-zsh
+			sudo git clone https://github.com/robbyrussell/oh-my-zsh /usr/share/oh-my-zsh
+
+		fi
 	;;
 	*)
-		echo -e "\n$($cyan)// woops. you're not running an Arch based distro\n"
+		echo -e "\n$($cyan)// woops. you're probably not running an $($yellow)Arch $($blue)or $($yellow)Ubuntu $($cyan)based distro$($reset)\n"
 	;;
 
 esac
+
+#cleanup leftovers
+rm -rf ~/.config/i3 ~/{.i3} ~/.fonts/{Font-Awesome,Inconsolata-for-Powerline}
+
+#make sure directories are present
+mkdir -p ~/{.fonts,.config}
+mkdir -p ~/Pictures/Screenshots
+
+#symlink all the goodies
+ln -s ~/dotfiles/.config/i3/ ~/.config/i3
+ln -s ~/dotfiles/.fonts/Font-Awesome/ ~/.fonts/Font-Awesome
+ln -s ~/dotfiles/.fonts/Inconsolata-for-Powerline ~/.fonts/Inconsolata-for-Powerline
+
+echo -e "\n$($cyan)// Backing up any previous $($yellow).Xresources $($cyan)& symlinking new one$($reset)\n"
+if [ -f ~/.Xresources ]; then
+	mv ~/.Xresources ~/.Xresources.old
+fi
+ln -s ~/dotfiles/.Xresources ~/.Xresources
+
+if [ "$zsh" = "y" ]; then
+
+	echo -e "\n$($cyan)// Backing up any previous $($yellow).zshrc $($cyan)& symlinking new one$($reset)\n"
+	if [ -f ~/.zshrc ]; then
+		mv ~/.zshrc ~/.zshrc.pre-oh-my-zsh
+	fi
+	ln -s ~/dotfiles/.zshrc ~/.zshrc
+
+	echo -e "\n$($cyan)// Changing default shell to $($yellow)zsh$($reset)\n"
+	chsh -s $(which zsh)
+fi
+
+echo -e "\n$($cyan)// All done. Make sure to \n	1.Set themes and fonts using $($yellow)lxappearance $($cyan)after logging into i3wm\n	2.Log out and back in for $($yellow)zsh $($cyan)to kick in\n	3.Maybe move useful code from $($yellow).zshrc.pre-oh-my-zsh $($cyan)or $($yellow).bashrc\n	$($cyan)4.Keep your old $($yellow).bashrc .zshrc.pre-oh-my-zsh .Xresources.old $($cyan)tucked off somewhere"
