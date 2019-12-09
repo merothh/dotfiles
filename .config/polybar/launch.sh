5a64143b9eec78268cfa6b9cc26201457babbfeb
merothh <merothh@gmail.com>
2019-12-09 15:23:00 +0530

    polybar: launch.sh: adapt to our names
    * as per our config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,9 +6,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 >>/tmp/polybar1.log 2>&1 &
-polybar bar2 >>/tmp/polybar2.log 2>&1 &
+# Launch bars top and bottom
+echo "---" | tee -a /tmp/polybar_top.log /tmp/polybar_bottom.log
+polybar top >>/tmp/polybar_top.log 2>&1 &
+polybar bottom >>/tmp/polybar_bottom.log 2>&1 &
 
 echo "Bars launched..."
