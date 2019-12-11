419b0faca38a10e152b1bc81adbeadc2a3436d02
merothh <merothh@gmail.com>
2019-12-11 23:20:57 +0530

    setup.sh: drop a couple of packages being installed
    * Drop the following:
    	- acpi
    	- adapta-gtk-theme
    	- alsa-utils
    	- i3blocks
    	- lsb-release
    	- sysstat
    	- xorg-xbacklight
    
    * Replace compton with picom

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ read zsh
 case $release in 
 	"Arch Linux"|"Antergos Linux"|"Manjaro Linux")
 
-		packages+=" i3-wm i3blocks i3lock rofi compton lsb-release scrot alsa-utils sysstat acpi feh playerctl xorg-xbacklight rxvt-unicode urxvt-perls adapta-gtk-theme adwaita-icon-theme papirus-icon-theme lxappearance otf-font-awesome noto-fonts bdf-unifont"
+		packages=" i3-wm i3lock rofi picom scrot feh playerctl rxvt-unicode urxvt-perls adwaita-icon-theme papirus-icon-theme lxappearance otf-font-awesome noto-fonts bdf-unifont"
 
 		echo -e "\n$($cyan)// Installing required packages$($reset)\n"
 		sudo pacman -S $packages
