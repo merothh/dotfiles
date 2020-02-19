b9a6bdc28343bb833892a2da0b11a82e3e3fe582
merothh <merothh@gmail.com>
2020-02-20 01:31:54 +0530

    polybar: stick with just the bottom bar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,9 +9,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bars top and bottom
+# Launch bar bottom
 echo "---" | tee -a /tmp/polybar_top.log /tmp/polybar_bottom.log
-polybar top >>/tmp/polybar_top.log 2>&1 &
 polybar bottom >>/tmp/polybar_bottom.log 2>&1 &
 
-echo "Bars launched..."
+echo "Bar launched..."
