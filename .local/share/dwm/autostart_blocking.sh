1d9964bda4d5b2d5416f84a06c9c33e0cfb967ea
merothh <merothh@gmail.com>
2020-12-04 08:09:05 +0530

    autostart_b*: open a spt instance

--- .local/share/dwm/autostart_blocking.sh
+++ .local/share/dwm/autostart_blocking.sh
@@ -1,3 +1,7 @@
+# kill existing processes (if any)
 killall -q dwmblocks
+kill $(xprop -name "spt" _NET_WM_PID | cut -d "=" -f 2)
+
 dwmblocks &
 feh --bg-scale ~/.local/share/dwm/wallpaper.jpg &
+urxvt -e spt &
