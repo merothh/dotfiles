511a845dfe31ef1e5a3878ac84328e47c2fb34f7
merothh <merothh@gmail.com>
2020-12-11 12:04:06 +0530

    autostart_blocking.sh: enable tapping for ELAN130 Touchpad

--- .local/share/dwm/autostart_blocking.sh
+++ .local/share/dwm/autostart_blocking.sh
@@ -5,3 +5,4 @@ kill $(xprop -name "spt" _NET_WM_PID | cut -d "=" -f 2)
 dwmblocks &
 feh --bg-scale ~/.local/share/dwm/wallpaper.jpg &
 urxvt -e spt &
+xinput set-prop "ELAN1300:00 04F3:3057 Touchpad" 332 1 &
